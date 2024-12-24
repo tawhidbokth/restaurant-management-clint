cfa27a7e465c18e3f1438ca5b768f758e6934c7c
shantodeveloper <ahmedshanto690@gmail.com>
2024-12-23 19:28:30 -0800

    add titel img

--- src/Pages/AllFoods.jsx
+++ src/Pages/AllFoods.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import AllFoodsCard from './AllFoodsCard';
+import PurchaseFoods from './PurchaseFoods';
 
 const AllFoods = () => {
   const foods = useLoaderData();
   return (
     <div>
+      <div
+        className="relative w-full mb-5  h-[200px] bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url('https://i.ibb.co.com/cTCcpBZ/DALL-E-2024-12-23-19-10-48-A-beautifully-styled-restaurant-themed-banner-background-image-with-a-war.webp')",
+        }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
+          <h1 className="text-4xl md:text-6xl font-bold mb-4">
+            Welcome to All Foods Pages
+          </h1>
+          <p className="text-lg md:text-xl mb-6">
+            Explore The All Food And Purchase
+          </p>
+        </div>
+      </div>
       <div className="w-[1400px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
         {foods.map(food => (
           <AllFoodsCard key={food._id} food={food}></AllFoodsCard>
--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -68,34 +68,51 @@ const Gallery = () => {
   };
 
   return (
-    <div className="gallery grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4 mt-2">
-      {images.map((image, index) => (
-        <div
-          key={index}
-          className="relative group overflow-hidden rounded-lg"
-          onClick={() => openLightbox(index)}
-        >
-          <img
-            src={image.src}
-            alt={image.description}
-            className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-105"
-          />
-
-          <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            <h3 className="text-white font-semibold">{image.name}</h3>
-            <p className="text-white text-sm">{image.description}</p>
-          </div>
+    <div>
+      <div
+        className="relative w-full mb-5  h-[200px] bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url('https://i.ibb.co.com/cTCcpBZ/DALL-E-2024-12-23-19-10-48-A-beautifully-styled-restaurant-themed-banner-background-image-with-a-war.webp')",
+        }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
+          <h1 className="text-4xl md:text-6xl font-bold mb-4">
+            Welcome Foods Gallery
+          </h1>
+          <p className="text-lg md:text-xl mb-6">See The All Photo And veiw</p>
         </div>
-      ))}
+      </div>
+      <div className="gallery grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4 mt-2">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="relative group overflow-hidden rounded-lg"
+            onClick={() => openLightbox(index)}
+          >
+            <img
+              src={image.src}
+              alt={image.description}
+              className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-105"
+            />
 
-      {open && (
-        <Lightbox
-          open={open}
-          close={() => setOpen(false)}
-          slides={images.map(image => ({ src: image.src }))}
-          index={currentIndex}
-        />
-      )}
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <h3 className="text-white font-semibold">{image.name}</h3>
+              <p className="text-white text-sm">{image.description}</p>
+            </div>
+          </div>
+        ))}
+
+        {open && (
+          <Lightbox
+            open={open}
+            close={() => setOpen(false)}
+            slides={images.map(image => ({ src: image.src }))}
+            index={currentIndex}
+          />
+        )}
+      </div>
     </div>
   );
 };
